ef0f08f133f246277444c34a8313029cf999dba0
JuliaMaksiutova <juliakolyvanova@gmail.com>
2022-12-01 22:54:12 +0500

    Made Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -7,7 +7,7 @@
 Console.Write("Введите цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-if(dayNumber == 6 || dayNumber == 6)
+if(dayNumber == 6 || dayNumber == 7)
 {
     Console.WriteLine("Этот день выходной");
 }
